28af14f8ea69c3d658eeece0a295ea5e7fa0ecb5
JiayiH74 <jiayih12@nycstudents.net>
2025-05-07 00:50:58 +0000

    done

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(APCalender.numberOfLeapYears(2000, 2025));
+        System.out.println(APCalender.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalender.dayOfWeek(1, 10, 2019));
+    }
+}
